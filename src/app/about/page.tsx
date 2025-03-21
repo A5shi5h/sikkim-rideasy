10a355826e851405c025dfdde272da8de831a764
Ashish Lamichaney <124287828+A5shi5h@users.noreply.github.com>
2025-03-21 11:21:01 +0530

    some change in about section

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -21,7 +21,7 @@ const Page = () => {
     <>
       {/* Lazy load BackgroundLines */}
       <Suspense fallback={<div className="text-center py-10">Loading...</div>}>
-        <BackgroundLines className="flex items-center justify-center w-full flex-col px-4">
+        <BackgroundLines className="flex items-center justify-center w-full flex-col px-4 max-md:mt-32">
           <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
             ABOUT
           </h2>
