cf33a9f18f3e795d456a62230fdcf0263a01229b
Ashish Lamichaney <124287828+A5shi5h@users.noreply.github.com>
2025-03-24 12:10:06 +0530

    change in tagline

--- src/components/TagLine.tsx
+++ src/components/TagLine.tsx
@@ -17,7 +17,7 @@ function TagLine() {
     <div className="relative h-[40rem] overflow-hidden flex items-center justify-center">
         <WavyBackground className="w-full max-w-7xl mx-auto flex flex-col items-center justify-center h-full">
             <h2 className="text-2xl md:text-4xl lg:text-7xl text-white font-bold text-center mb-8">Sikkim RidEasy</h2>
-            <p className="text-base md:text-xl text-white text-center mb-4">Don't be slow ! Our prices are low</p>
+            <p className="text-base md:text-xl text-white text-center mb-4 font-light">Ride Easy , Pay Less , Explore More !</p>
             <div className="flex flex-row items-center justify-center mb-10 w-full">
                 <AnimatedTooltip items={tag} />
             </div>
